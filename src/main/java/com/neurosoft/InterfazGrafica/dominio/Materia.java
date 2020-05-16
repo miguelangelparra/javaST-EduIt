ef02e7a6d08fa39c0d4f00ef3051fe456920f42e
miguelangelparra <miguelangel.parradavila@gmail.com>
2020-05-16 12:21:12 -0300

    Inicio Interfaz Grafica

---
+++ src/main/java/com/neurosoft/InterfazGrafica/dominio/Materia.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neurosoft.InterfazGrafica.dominio;
+
+import com.neurosoft.BaseDeDatos.*;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author m.parra.davila
+ */
+public class Materia extends Entidad {
+
+    private String nombre;
+    private int cargaHoraria;
+    private List<Carrera> carreras;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public List<Carrera> getCarreras() {
+        return carreras;
+    }
+
+    public void setCarreras(List<Carrera> carreras) {
+        this.carreras = carreras;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        hash = 29 * hash + this.cargaHoraria;
+        hash = 29 * hash + Objects.hashCode(this.carreras);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Materia other = (Materia) obj;
+        if (this.cargaHoraria != other.cargaHoraria) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.carreras, other.carreras)) {
+            return false;
+        }
+        return true;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "id=" + this.getId() + ", nombre=" + nombre + ", cargaHoraria=" + cargaHoraria + ", carreras=" + carreras + '}';
+    }
+
+}
---
+++ src/main/java/com/neurosoft/InterfazGrafica/persistencia/MateriaDao.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neurosoft.InterfazGrafica.persistencia;
+
+import com.neurosoft.BaseDeDatos.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author m.parra.davila
+ */
+public class MateriaDao extends DaoSupport<Materia> {
+
+    private ConnectionManager connectionManager;
+
+    public MateriaDao(ConnectionManager connectionManager) {
+        super(connectionManager);
+    }
+
+    @Override
+    protected String sqlActualizar(Materia entidad) {
+        return "UPDATE materia set nombre= ? , carga_horaria= ? WHERE id = ?";
+    }
+
+    @Override
+    protected Materia createEntity(ResultSet resultSet) throws SQLException {
+        Materia materia = new Materia();
+        materia.setId(resultSet.getInt("id"));
+        materia.setNombre(resultSet.getString("nombre"));
+        materia.setCargaHoraria(resultSet.getInt("carga_horaria"));
+        return materia;
+    }
+
+    @Override
+    protected String tableName() {
+        return "materia";
+    }
+
+    @Override
+    protected String sqlInsert(Materia entidad
+    ) {
+        return "insert into Materia (nombre,carga_horaria) values(?,?)";
+    }
+
+    @Override
+    protected Map<Integer, Object> getParameters(Materia entidad
+    ) {
+        Map<Integer, Object> parametros = new HashMap<>();
+        parametros.put(1, entidad.getNombre());
+        parametros.put(2, entidad.getCargaHoraria());
+        return parametros;
+    }
+
+    /*
+    @Override
+    public Materia grabar(Materia entidad) throws SQLException {
+
+        String sql = "insert into materia(nombre,carga_horaria¨) value (?,?)";
+        PreparedStatement stmt = connectionManager.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, entidad.getNombre());
+        stmt.setInt(2, entidad.getCargaHoraria());
+        stmt.executeUpdate();
+
+        ResultSet keys = stmt.getGeneratedKeys();
+        if (keys.next()) {
+            entidad.setId(keys.getInt(1));
+        }
+        return entidad;
+    }
+
+    @Override
+    public void actualizar(Materia entidad) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Integer id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Materia obtenerUno(Integer id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Materia> obtenerTodos() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     */
+}
